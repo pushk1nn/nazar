9af213721d59d154c0ea7c36bd9f3c5a37cbb0ce
maximka <mvkochnev42@gmail.com>
2025-10-29 16:21:15 -0400

    Fix interface env variable?

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt install libpcap-dev gcc
 export CGO_ENABLED=1
 
 # Configure env variables
-export IFACE=$1
+export IFACE="$1"
 echo "[+] Set to listen on interface $1"
 
 # Build and set binary capabilities
