c186f2207161409baa77fe48a4d12df06dd4d878
maximka <mvkochnev42@gmail.com>
2025-10-29 16:15:01 -0400

    Make device an argument to Listen()

--- logging/packets.go
+++ logging/packets.go
@@ -2,20 +2,16 @@ package logging
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
-// device gets the environment variable to listen on
-var device = os.Getenv("IFACE")
-
 // Listen will initialize the interface to be used for packet captures,
 // then start the loop to handle packets.
-func Listen() {
-	if handle, err := pcap.OpenLive(device, 1600, true, pcap.BlockForever); err != nil {
+func Listen(dev string) {
+	if handle, err := pcap.OpenLive(dev, 1600, true, pcap.BlockForever); err != nil {
 		panic(err)
 	} else if err := handle.SetBPFFilter("tcp src port 22"); err != nil { // For early development, listen for ssh
 		panic(err)
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"github.com/pushk1nn/nazar/logging"
 )
 
+// device gets the environment variable to listen on
+var device = os.Getenv("IFACE")
+
 func main() {
 	// Start listening for new packets
-	logging.Listen()
+	logging.Listen(device)
 }
